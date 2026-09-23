78d2a9724f49b721e8b319dae5e54d17e4810e30
Jubayer Ahmed <mdjubayer444666@gmail.com>
2026-09-23 22:58:34 +0600

    chore: bump version to 2.0.1 (versionCode 11) and fix website hydration warning

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -29,8 +29,9 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <AuthProvider>
           <AuthGuard>{children}</AuthGuard>
         </AuthProvider>
